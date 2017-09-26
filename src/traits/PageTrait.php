2f21e33277ca4a57d7cdbdb60581c9c2ec4973c9
chenzeshu <1193297950@qq.com>
2017-09-26 17:34:27 +0800

    利用递归优化了PageTrait, 可以下探到Models文件夹最底层

--- src/traits/PageTrait.php
+++ src/traits/PageTrait.php
@@ -11,52 +11,80 @@ namespace Chenzeshu\ChenUtils\Traits;
 
 trait PageTrait
 {
+    /**
+     * @param $page 开始的页码, 不用减一
+     * @param $pageSize 单页显示数据数目
+     * @return mixed
+     */
     public function getPaginator($page, $pageSize){
         $begin = ($page - 1) * $pageSize;
 
         $arr = explode('\\',get_class($this));
         $start = strpos($arr[count($arr)-1] ,'Controller' );
         $classname = substr_replace($arr[count($arr)-1], '', $start);
-        //静态绑定
-        //读取Models文件目录结构, 匹配$classname, 若匹配到, 取得匹配文件的namespace组装后进行静态绑定
-        //todo if Laravel 使用Storage类
+
         return $this->matchFile($classname, $begin, $pageSize);
     }
 
     /**
      * @param $classname 我们想要找的文件(无.php, 要加)
+     * @param $begin 开始的数据条数
+     * @param $pageSize 单页显示数据数目
+     * @return mixed 一般返回的是分页API数据
      */
     public function matchFile($classname, $begin, $pageSize){
-        $files = scandir(app_path('/Models'));
-        //todo 只要是.php结尾的就是类文件, 否则是文件夹
-        //所以第一遍循环.php文件, 是否匹配, 若无匹配, 再向下一层
-        //todo first circle
-
-        $flag = true;  //有没有必要向下一层
-        $filename = $classname. '.php';
-        $root = "App\\Models\\";
-        //递归寻找匹配的.php文件
-        foreach ($files as $file){
-            if($file == $filename){
-                $myModel =  $root . $classname;
-                $flag = false;
-            }
+        if($classname == 'User'){
+            $myModel = "App\\User";
         }
+        else {
+            $filename = $classname. '.php';
+            $modeldir = dirname(app_path('/models'))."/models";
+            //todo 首先得到app目录下的文件目录结构数组
+            $absolutePaths = $this->getDir($modeldir);
 
-        if($flag){
-            //todo 向没有.php结尾的下一层进发
-            foreach ($files as $file){
-                if(!strpos($file, '.php')){
-                    $_files = scandir(app_path('/Models/' .$file));
-                    foreach($_files as $_file){
-                        if($_file == $filename){
-                            $myModel = "App\\Models\\" . $file."\\". $classname;
-                        }
-                    }
+            foreach ($absolutePaths as $k =>$path){
+                $absolutePaths[$k] = str_replace($modeldir,'',$path);
+                //todo 得到.php文件并与filename比对
+                $arr = explode('/',$absolutePaths[$k]);
+                if($arr[count($arr)-1] == $filename){
+                    $myModel = str_replace('/', '', "App\\Models\\".str_replace('.php', '', $absolutePaths[$k]));
                 }
             }
         }
+
         $data = (new $myModel)->offset($begin)->limit($pageSize)->get();
+        return $data;
+    }
+
+    /**
+     * 可以查到目录下所有文件的绝对路径, 形成数组返回
+     * @param $path 要查文件的目录路径(绝对路径)
+     * @param $data 一个空数组
+     * 注意: 这里在$data前加引用, 主要是引用了getDir()的$data, 保证了数据是数组格式
+     */
+    public function searchDir($path,&$data){
+        if(is_dir($path)){
+            $dp=dir($path);
+            while($file=$dp->read()){
+                if($file!='.'&& $file!='..'){
+                    $this->searchDir($path.'/'.$file,$data);
+                }
+            }
+            $dp->close();
+        }
+        if(is_file($path)){
+            $data[]=$path;
+        }
+    }
+
+    /**
+     * 简单的调用
+     * @param $dir  要查的目录路径
+     * @return array
+     */
+    public function getDir($dir){
+        $data=array();
+        $this->searchDir($dir,$data);
         return $data;
     }
 }
