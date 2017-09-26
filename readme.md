0297859e2af497ae3b271f54f522a6c03458bd4e
chenzeshu <1193297950@qq.com>
2017-09-26 15:55:59 +0800

    PageTrait

--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@
     3.use for curl
     4.use for value-check
     5.use for json-return-format
-
+    6.use for auto_api_age
 ## facades
     1. ChenUtilFacade
        - getWechatOpenid & session-key
@@ -14,3 +14,23 @@
     expect to do :
        - apis for wechatPay
    
+## 各个traits的用法
+### 1. PageTrait
+用途: 在不同的**控制器**中, 通过调用一个方法直接得到适用于api的分页数据
+
+```
+use PageTrait;
+
+/**
+* $page int 起始页码, 无需减一
+* $pageSize int, 每页展示的数据量
+*/
+$this->getPaginator($page, $pageSize);
+```
+
+问题1: 目前就做了三层, 从app目录下, 到app/models目录下, 再到下一级
+暂时没有递归文件夹的思路
+
+或者一次性得到所有的文件(非目录)的路径数组, 然后匹配文件名, 匹配中的返回文件路径, 进而得到命名空间,也可以.
+
+问题2: 如分类连表查询等还没有做
---
+++ src/traits/PageTrait.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/26
+ * Time: 15:46
+ */
+
+namespace Chenzeshu\ChenUtils\Traits;
+
+
+trait PageTrait
+{
+    public function getPaginator($page, $pageSize){
+        $begin = ($page - 1) * $pageSize;
+
+        $arr = explode('\\',get_class($this));
+        $start = strpos($arr[count($arr)-1] ,'Controller' );
+        $classname = substr_replace($arr[count($arr)-1], '', $start);
+        //静态绑定
+        //读取Models文件目录结构, 匹配$classname, 若匹配到, 取得匹配文件的namespace组装后进行静态绑定
+        //todo if Laravel 使用Storage类
+        return $this->matchFile($classname, $begin, $pageSize);
+    }
+
+    /**
+     * @param $classname 我们想要找的文件(无.php, 要加)
+     */
+    public function matchFile($classname, $begin, $pageSize){
+        $files = scandir(app_path('/Models'));
+        //todo 只要是.php结尾的就是类文件, 否则是文件夹
+        //所以第一遍循环.php文件, 是否匹配, 若无匹配, 再向下一层
+        //todo first circle
+
+        $flag = true;  //有没有必要向下一层
+        $filename = $classname. '.php';
+        $root = "App\\Models\\";
+        //递归寻找匹配的.php文件
+        foreach ($files as $file){
+            if($file == $filename){
+                $myModel =  $root . $classname;
+                $flag = false;
+            }
+        }
+
+        if($flag){
+            //todo 向没有.php结尾的下一层进发
+            foreach ($files as $file){
+                if(!strpos($file, '.php')){
+                    $_files = scandir('/Models/'.$file);
+                    foreach($_files as $_file){
+                        if($_file == $filename){
+                            $myModel = "App\\Models\\" . $file."\\". $classname;
+                        }
+                    }
+                }
+            }
+        }
+        $data = (new $myModel)->offset($begin)->limit($pageSize)->get();
+        return $data;
+    }
+}
